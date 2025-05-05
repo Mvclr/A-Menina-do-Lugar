a0e8aae53a9e3810b7e7b53210e6760763d790ea
Manoel <mvclr1@gmail.com>
2025-05-05 20:48:48 -0300

    jsx placed together to pass to main

---
+++ src/App/AppMain.jsx
@@ -0,0 +1,18 @@
+import Header from "../header/Header";
+import Footer from "../footer/Footer";
+import Book from "../books/books";
+import AboutUs from "../AboutUs/AboutUs";
+import ContactUs from "../ContactUs/ContactUs";
+function App() {
+  return (
+    <>
+      <Header />
+      <AboutUs/>
+      <Book />
+      <ContactUs/>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
