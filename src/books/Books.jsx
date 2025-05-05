f019b72f2f85510ad3c5299f46a3241b34235ce4
Manoel <mvclr1@gmail.com>
2025-05-05 20:49:55 -0300

    Books section
    Books section jsx and css

---
+++ src/books/Books.jsx
@@ -0,0 +1,24 @@
+import styles from './Books.module.css'
+import books from './booksArray.jsx'
+
+function Book() {
+  return (
+    <section className={styles.sectionAll} id='Home'>
+      <h1>Livros</h1>
+      <main className={styles.imageSection}>
+        {books.map((book, index) => (
+          <div key={index} className={styles.imgDiv}>
+            <span className={styles.bookTitle}>{book.name}</span>
+            <img 
+              src={book.imgUrl} 
+              alt={book.name}
+              className={styles.bookImage}
+            />
+          </div>
+        ))}
+      </main>
+    </section>
+  );
+}
+
+export default Book;
