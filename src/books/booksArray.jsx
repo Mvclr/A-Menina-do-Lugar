9868ba0598a5d3b44e225f82f17c94ca64d1fc4f
Manoel <mvclr1@gmail.com>
2025-05-05 20:50:29 -0300

    books array
    Books url and id, used in the book section

---
+++ src/books/booksArray.jsx
@@ -0,0 +1,24 @@
+const books=[
+    {
+        "name": "A Menina do Lugar - Muquém",
+        "imgUrl": "src/assets/CapaLivros/Livro-Muquem-Capa.png",
+        "id": 1
+    },
+    {
+        "name": "A Menina do Lugar - Viçosa",
+        "imgUrl": "src/assets/CapaLivros/Livro-Viçosa-Capa.png",
+        "id": 2
+    },
+    {
+        "name": "A Menina do Lugar - Cafurna",
+        "imgUrl": "src/assets/CapaLivros/Livro-Cafurna-Capa.png",
+        "id": 3
+    },
+    {
+        "name": "A Menina do Lugar - Coruripe",
+        "imgUrl": "src/assets/CapaLivros/Livro-Coruripe-Capa.png",
+        "id": 4
+    }
+]
+
+export default books
