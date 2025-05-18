d9c9c7c287709d15ddcfd4d005b145e5b81ace10
Manoel <mvclr1@gmail.com>
2025-05-18 01:32:19 -0300

    Update on the book section

--- src/App/BookPage.jsx
+++ src/App/BookPage.jsx
@@ -2,14 +2,14 @@ import Header from "../header/Header";
 import Footer from "../footer/Footer";
 import Book from "../books/books";
 import AboutUs from "../AboutUs/AboutUs";
-import FallingLeaves from "../FallingLeaves/FallingLeaves";
-import JogoCocada from "../GameCode/game";
+import CoruripeGame from "../GameCode/Coruripe/gameCoruripe";
+import CafurnaGame from "../GameCode/Cafurna/gameCafurna";
 function BookPage() {
   return (
     <>
       <Header />
-      <JogoCocada/>
-      <Footer />
+      <CafurnaGame/>
+      <Footer/>
     </>
   );
 }
--- src/books/Books.jsx
+++ src/books/Books.jsx
@@ -2,6 +2,22 @@ import styles from './Books.module.css'
 import books from './booksArray.jsx'
 
 function Book() {
+
+  async function handleClick() {
+    try {
+      const response = await fetch(
+        '/bookPage', 
+        {method: 'GET'}
+      );
+      if (!response.ok) {
+        throw new Error('Sem resposta do servidor');
+      }
+      console.log('Resposta do servidor:', response);
+    } catch (error) {
+      console.error('Erro chamando endpoint /bookPage:', error);
+    }
+  }
+
   return (
     <section id='Home'>
       <h1>Livros</h1>
@@ -10,6 +26,7 @@ function Book() {
           <div key={index} className={styles.imgDiv}>
             <span className={styles.bookTitle}>{book.name}</span>
             <img 
+              onClick={handleClick}
               src={book.imgUrl} 
               alt={book.name}
               className={styles.bookImage}
