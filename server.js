11ef21a691e96763ca536b12fd96d60bfd326aa1
Manoel <mvclr1@gmail.com>
2025-05-18 01:30:02 -0300

    Add bookPage endpoint
    New GET endpoint, returning the specific page of the books

--- server.js
+++ server.js
@@ -21,10 +21,13 @@ app.get('/api/books', (req, res) => {
   ]);
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/bookPage', (req, res) => {
+  res.sendFile(path.join(__dirname, 'bookCafurna.html'));
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
