00f12ada3729ae82813b8fef9abb4e5e3c793853
Manoel <mvclr1@gmail.com>
2025-08-11 03:24:19 -0300

    Initialize React app with Vite and Tailwind setup
    Added initial project files including a README, index.html update to use src/index.jsx, base App.jsx component, and Tailwind CSS setup in index.css. This sets up the foundation for a React project using Vite and Tailwind CSS.

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import HomePage from "./pages/MainPage.jsx";
+import GamePage from "./pages/GamePage.jsx";
+function App() {
+  return <HomePage/>;
+}
+
+export default App;
